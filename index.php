9d612604047b79ccf79a654e3d9a4f930f7203ee
dmm04 <dmm042105@gmail.com>
2024-12-10 22:26:09 -0500

    implementing the product section with placeholder images and titles

--- index.php
+++ index.php
@@ -7,3 +7,9 @@
 <body>
     <!--Hero Section -->
     <section class="hero" style="background-image: url('basket.jpg');">
+        <div class="hero-content">
+            <h1>Welcome to Jamit! Baskets</h1>
+            <p>Unique Handmade Baskets For Every Occasion</p>
+            <a href="shop.html" class="cta-button">Shop Now</a>
+        </div>
+            
